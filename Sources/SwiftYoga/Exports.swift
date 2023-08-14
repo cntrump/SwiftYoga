1fa69b8a0fa379d26d30283efe1acb13df5ba90e
Lvv.me <cntrump@gmail.com>
2023-08-14 16:48:34 +0800

    Export necessary structs from yoga

--- Sources/SwiftYoga/Exports.swift
+++ Sources/SwiftYoga/Exports.swift
@@ -1,3 +1,11 @@
 
 @_exported import Foundation
-@_exported import yoga
+@_exported import struct yoga.core.YGDirection
+@_exported import struct yoga.core.YGFlexDirection
+@_exported import struct yoga.core.YGJustify
+@_exported import struct yoga.core.YGAlign
+@_exported import struct yoga.core.YGPositionType
+@_exported import struct yoga.core.YGWrap
+@_exported import struct yoga.core.YGOverflow
+@_exported import struct yoga.core.YGDisplay
+@_exported import struct yoga.core.YGValue
